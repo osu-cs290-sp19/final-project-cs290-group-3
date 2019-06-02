4ab5d368b2a45821a9dc24c8298add26ba29b18a
Jack McConnell <mcconnja@oregonstate.edu>
2019-06-02 13:56:07 -0700

    Made everything a template

---
+++ public/posttemp.js
@@ -0,0 +1,12 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['posttemp'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<article class=\"post\">\r\n  <div class=\"image\">\r\n    <img class=img\" src=\""
+    + alias4(((helper = (helper = helpers.img || (depth0 != null ? depth0.img : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"img","hash":{},"data":data}) : helper)))
+    + "\" />\r\n  </div>\r\n  <div class=\"content\">\r\n    <p class=\"text\">\r\n      "
+    + alias4(((helper = (helper = helpers.txt || (depth0 != null ? depth0.txt : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"txt","hash":{},"data":data}) : helper)))
+    + "\r\n    </p>\r\n  </div>\r\n</article>\r\n";
+},"useData":true});
+})();
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var path = require('path');
 var express = require('express');
-
+var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
-
+var data = require('./data');
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.use(express.static('public'));
+
+app.get('/home', function (req, res, next) {
+  res.status(200).render('homepage', {data: data}) //data?
+});
 
 app.get('*', function (req, res) {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
