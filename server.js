6f673ab057244251199f02b017f0bb01011731fc
Kirkpary <kirkpary@oregonstate.edu>
2019-06-08 19:16:52 -0700

    Made some progress on reply database.

--- server.js
+++ server.js
@@ -26,13 +26,24 @@ app.use(express.static('public'));
 
 app.get('/', function (req, res, next) {
   var collection = db.collection('postData');
-  var postArray = collection.find({ pageTitle: "Home" }).toArray(function(err, pageData) {
+  var pageTitle = "Home";
+  var postId = 0;
+  collection.find({ pageTitle: pageTitle, "posts.postId": postId }).toArray(function(err, post) {
+    if (err) {
+      res.status(500).send({
+        error: "Error fetching post from DB"
+      });
+    } else {
+      console.log("== Post:", post);
+    }
+  });
+  var postArray = collection.findOne({ pageTitle: "Home" }, function(err, pageData) {
     if (err) {
       res.status(500).send({
         error: "Error fetching people from DB"
       });
     } else {
-      res.status(200).render('homepage', pageData[0]);
+      res.status(200).render('homepage', pageData);
     }
   });
 });
@@ -41,14 +52,14 @@ app.get('/:pageTitle', function (req, res, next) {
   var pageTitle = req.params.pageTitle;
   pageTitle = parsePageTitle(pageTitle);
   var collection = db.collection('postData');
-  var postArray = collection.find({ pageTitle: pageTitle }).toArray(function(err, pageData) {
+  var postArray = collection.findOne({ pageTitle: pageTitle }, function(err, pageData) {
     if (err) {
       res.status(500).send({
-        error: "Error fetching people from DB"
+        error: "Error fetching page from DB"
       });
     } else {
       console.log("== pageData:", pageData);
-      res.status(200).render('homepage', pageData[0]);
+      res.status(200).render('homepage', pageData);
     }
   });
 });
@@ -79,12 +90,37 @@ app.post('/:pageTitle/addPost', function(req, res, next) {
 
 app.post('/:pageTitle/:postId/addReply', function(req, res, next) {
   if(req.body && req.body.text) {
+    var pageTitle = parsePageTitle(req.params.pageTitle);
+    var postId = req.params.postId;
     console.log("== Client added the following reply:");
     console.log("   - pageTitle:", req.params.pageTitle);
     console.log("   - postId:", req.params.postId);
     console.log("   - text:", req.body.text);
 
-    // Add reply to DB here
+    var collection = db.collection('postData');
+    var reply = {
+      text: req.body.text
+    };
+
+    collection.updateOne(
+      { pageTitle: pageTitle, "posts.postId": postId },
+      { $push: { "posts.$.replies": reply } },
+      function (err, result) {
+        if (err) {
+          res.status(500).send({
+            error: "Error inserting reply into DB"
+          });
+        } else {
+          //console.log("== update result:", result);
+          if (result.matchedCount > 0) {
+            console.log("== success");
+            res.status(200).send("Success");
+          } else {
+            next();
+          }
+        }
+      }
+    );
 
     res.status(200).send("Reply successfully added");
   } else {
